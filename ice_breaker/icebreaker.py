0cc014ee98d3bf305c4ac8b79406a9f8d636270e
Shashi Kiran <61668526+kiransview@users.noreply.github.com>
2023-05-23 23:33:43 +0530

    First_commit

---
+++ ice_breaker/icebreaker.py
@@ -0,0 +1,26 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+from third_party.linkedin import scrape_linkedin_profile
+
+if __name__ == "__main__":
+    print("Hello Langchain")
+
+    summary_template = """
+        given LinkedIn information {information} about a person from I want to create:
+        1. a short summary
+        2. two interesting facts about them
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/naikkrish/"
+    )
+
+    print(chain.run(information=linkedin_data))
